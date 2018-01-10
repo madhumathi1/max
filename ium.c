3aec74519a19f19edcbd5f6c512f5d2f134ce77c
madhumathi1 <madhumathid28@gmail.com>
2018-01-10 07:08:48 -0800

    Create ium.c

---
+++ ium.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int main()
+{
+  int n,arr[n],i,max=arr[0];
+  scanf("%d",&n);
+  for(i=0;i<n;i++)
+  {
+      scanf("%d",&arr[i]);
+  }
+  for(i=1;i<n;i++)
+  {
+      if(arr[i]>max)
+      {
+          max=arr[i];
+      }
+    }
+ printf("%d",max);
+ return 0;
+}
